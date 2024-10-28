86ca1249d942ef8854eee0904b8dc51fb5056683
exLinnn <lin-jf21@mails.tsinghua.edu.cn>
2024-10-28 21:56:34 +0800

    add jupyter presentation

--- jupyter/sql.py
+++ jupyter/sql.py
@@ -7,7 +7,7 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS Results (
         id INTEGER PRIMARY KEY,
         model_name TEXT,
-        task_type TEXT CHECK(task_type IN ('classification', 'forecast', 'anomaly')),
+        task_type TEXT CHECK(task_type IN ('classification', 'long-term-forecast', 'short-term-forecast', 'anomaly')),
         dataset_name TEXT,
         seq_len INTEGER,
         metric TEXT,
@@ -16,8 +16,9 @@ cursor.execute('''
 ''')
 
 data = [
-    ('TimesNet', 'forecast', 'ECL', 96, 'MSE', 0.168),
-    ('DLinear', 'forecast', 'ECL', 96, 'MSE', 0.197)
+    ('TimesNet', 'long-term-forecast', 'ECL', 96, 'MSE', 0.168),
+    ('DLinear', 'long-term-forecast', 'ECL', 96, 'MSE', 0.197),
+    ('Informer', 'long-term-forecast', 'ECL', 96, 'MSE', 0.274),
 ]
 
 cursor.executemany('''
